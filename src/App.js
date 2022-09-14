5ba3071cd82f697040bcc736f371530652dbcaee
Dam <shit2wice@gmail.com>
2022-09-14 18:44:51 +0100

    chore: Add custom variables

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Welcome to the playground follow me.</div>;
+  return (
+    <div className="app">
+      Welcome to the playground follow me
+      <div className="app__header">{/* Header */}</div>
+      <div className="app__hero">{/* Hero  */}</div>
+      <div className="app__main">
+        <div className="main-shortener">{/* Shortener  */}</div>
+        <div className="main-stats">{/* Stats  */}</div>
+      </div>
+      <div className="app__cta">{/* CTA  */}</div>
+      <div className="app__footer">{/* Footer  */}</div>
+    </div>
+  );
 }
 
 export default App;
