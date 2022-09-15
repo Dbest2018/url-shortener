e1cfad88a7b28fc5934f99a5fc4bf6742501412f
Dam <shit2wice@gmail.com>
2022-09-15 17:25:31 +0100

    chore: Add hmtl content to shortener component

--- src/components/Shortener/Shortener.js
+++ src/components/Shortener/Shortener.js
@@ -2,7 +2,16 @@ import React from "react";
 import "./Shortener.css";
 
 const Shortener = () => {
-  return <div>Shortener</div>;
+  return (
+    <div className="shortener">
+      <input
+        className="shortener__input"
+        type="text"
+        placeholder="Shorten a link here..."
+      />
+      <div className="shortener__button">Shorten It!</div>
+    </div>
+  );
 };
 
 export default Shortener;
