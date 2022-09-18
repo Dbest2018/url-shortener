42cf764feb919be5cad218a23dd62909aaab3b95
Dam <shit2wice@gmail.com>
2022-09-18 12:59:46 +0100

    style: Add shortener to desktop layout

--- src/components/Shortener/Shortener.js
+++ src/components/Shortener/Shortener.js
@@ -4,11 +4,13 @@ import "./Shortener.css";
 const Shortener = () => {
   return (
     <div className="shortener">
-      <input
-        className="shortener__input"
-        type="text"
-        placeholder="Shorten a link here..."
-      />
+      <div className="shortener-test">
+        <input
+          className="shortener__input"
+          type="text"
+          placeholder="Shorten a link here..."
+        />
+      </div>
       <div className="shortener__button">Shorten It!</div>
     </div>
   );
