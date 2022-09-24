612206d7d91df9238dac233f3c50cd2b032db78a
Dam <shit2wice@gmail.com>
2022-09-24 23:09:43 +0100

    style: Modify button color on click

--- src/components/Short/Short.js
+++ src/components/Short/Short.js
@@ -3,17 +3,26 @@ import "./Short.css";
 
 const Short = ({ url }) => {
   const [buttonText, setButtonText] = useState("Copy");
+  const [isCopied, setIsCopied] = useState(false);
 
+  const buttonStyle = {
+    backgroundColor: isCopied && "var(--dviolet)",
+  };
   const handleClick = () => {
     navigator.clipboard.writeText(url.shortUrl);
     setButtonText("Copied");
+    setIsCopied(true);
   };
   return (
     <div className="short">
       <div className="short__long">{url.longUrl}</div>
       <div className="short__right">
         <div className="short__short">{url.shortUrl}</div>
-        <div className="short__button" onClick={handleClick}>
+        <div
+          className="short__button"
+          onClick={handleClick}
+          style={buttonStyle}
+        >
           {buttonText}
         </div>
       </div>
