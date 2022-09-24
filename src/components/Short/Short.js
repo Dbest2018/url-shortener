64daf79ae3ac0715bfdfa719f4b09233de841792
Dam <shit2wice@gmail.com>
2022-09-24 23:08:54 +0100

    feat: Add shortened url to clipboard

--- src/components/Short/Short.js
+++ src/components/Short/Short.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Short.css";
 
 const Short = ({ url }) => {
+  const [buttonText, setButtonText] = useState("Copy");
+
+  const handleClick = () => {
+    navigator.clipboard.writeText(url.shortUrl);
+    setButtonText("Copied");
+  };
   return (
     <div className="short">
       <div className="short__long">{url.longUrl}</div>
       <div className="short__right">
         <div className="short__short">{url.shortUrl}</div>
-        <div className="short__button">copy</div>
+        <div className="short__button" onClick={handleClick}>
+          {buttonText}
+        </div>
       </div>
     </div>
   );
